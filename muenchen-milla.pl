2c53e8537fedb3d9075a95ff521186e506315af5
Bernhard Geier <2651739+citronalco@users.noreply.github.com>
2023-03-09 15:59:29 +0100

    muenchen-milla.pl: Has new web page

--- muenchen-milla.pl
+++ muenchen-milla.pl
@@ -22,7 +22,7 @@ use Data::Dumper;
 use warnings;
 
 
-my $url="https://www.milla-club.de/category/event/";
+my $url="https://www.milla-club.de/";
 my @monatsnamen=('jan','feb','mar','apr','may','jun','jul','aug','sep','oct','nov','dec');
 
 my $defaultDauer=119;   # angenommene Dauer eines Events in Minuten (steht nicht im Programm, wird aber für Kalendereintrag gebraucht)
@@ -32,122 +32,145 @@ binmode STDOUT, ":utf8";	# Gegen "wide character"-Warnungen
 
 my $mech=WWW::Mechanize->new();
 $mech->get($url) or die($!);
-my $root=HTML::TreeBuilder->new_from_content($mech->content());
+
+my $root=HTML::TreeBuilder->new();
+$root->ignore_unknown(0);       # "section"-Tag wird sonst nicht erkannt
+$root->parse_content($mech->content());
 
 my @eventList;
 
-foreach my $eventBox ($root->look_down('_tag'=>'div','class'=>'preview-box-wrapper')) {
-    my $event;
+# Monatsweise durchgehen um "Featured" zu überspringen
+foreach my $monthSection ($root->look_down('_tag'=>'section','class'=>'events')) {
+    foreach my $eventItem ($monthSection->look_down('_tag'=>'div','class'=>'event__title')) {
 
-    ## Titel, URL, Kategorie
-    my $titleLink=$eventBox->look_down('_tag'=>'h3','class'=>'post-title');
-    ($event->{'kategorie'},$event->{'titel'})=$titleLink->as_trimmed_text()=~/(.+): (.+)/;
-    $event->{'titel'}=$titleLink->as_trimmed_text() unless ($event->{'titel'});
-    $event->{'url'}=$titleLink->look_down('_tag'=>'a')->attr('href');
-
-    ## Datum
-    my $year=$eventBox->look_down('_tag'=>'span','class'=>'post-date-year')->as_trimmed_text();
-    # Monatsnummer aus Monatsname
-    my $monthname=$eventBox->look_down('_tag'=>'span','class'=>'post-date-month')->as_trimmed_text();
-    my $month=1+first_index { $_ eq lc($monthname) } @monatsnamen;
-    # Tag
-    my $day=$eventBox->look_down('_tag'=>'span','class'=>'post-date-day')->as_trimmed_text();
-    $event->{'datum'}=$day.".".$month.".".$year;
-
-    ## Tickets
-    try {
-	$event->{'tickets'}=$eventBox->look_down('_tag'=>'div','class'=>'reserve')->look_down('_tag'=>'a')->attr('href');
-    };
-
-    ## Unterseite aufrufen
-    try {
-	$mech->get($event->{'url'});
-    }
-    catch {
-	next;
-    };
-    my $page=HTML::TreeBuilder->new();
-    $page->ignore_unknown(0);       # "article"-Tag wird sonst nicht erkannt
-    $page->parse_content($mech->content());
-
-    ## Beschreibungstext
-    my $text_html=$page->look_down('_tag'=>'div','class'=>'text-wrapper')->as_HTML();
-    my $text=$page->look_down('_tag'=>'div','class'=>'text-wrapper')->as_trimmed_text();
-
-    # Beschreibungstext hat normalerweise zwei Teile: Inhaltsbeschreibung und, nach ein paar Tildezeichen, Organisatorisches
-    my $orga;
-    if ($text=~/(.+?)\~\~\~\~\~\~+(.+)/) {
-	$event->{'beschreibung'}=$1;
-	$orga=$2;
-    }
-    else {
-	# Ohne Trenner kompletten Text als Beschreibung verwenden..
-	$event->{'beschreibung'}=$text;
-	# ...und versuchen, daraus Einlass, Beginn und Preise rauszufinden
-	$orga=$text;
-    }
-
-    # "Organisatorisches" enthält AK, VVK, Einlass, Beginn usw.
-    if ($orga=~/(VVK:?\s+[\d,\.]+\s€ zzgl\. Geb\.)/) {
-        $event->{'vvk'}=$1;
-    }
-    if ($orga=~/(AK:?\s+[\d,\.]+\s€)/) {
-        $event->{'ak'}=$1;
-    }
-
-    if ($orga=~/Einlass:?\s+(\d+)[:\.](\d+)/) {
-        $event->{'einlass'}=$datumFormat->parse_datetime($event->{'datum'}." ".$1.":".$2);
-    }
-    elsif ($orga=~/(\d+)[:\.](\d+) (?:Uhr )?Einlass/) {
-        $event->{'einlass'}=$datumFormat->parse_datetime($event->{'datum'}." ".$1.":".$2);
-    }
-    elsif ($orga=~/Einlass:?\s+(\d+)[:\.]/) {	# Minuten "00" wird gelegenlich als "oo" geschrieben...
-        $event->{'einlass'}=$datumFormat->parse_datetime($event->{'datum'}." ".$1.":00");
-    }
-    elsif ($orga=~/(\d+)[:\.].{2} Einlass/) {	# Minuten "00" wird gelegenlich als "oo" geschrieben, und "Einlass" danach
-        $event->{'einlass'}=$datumFormat->parse_datetime($event->{'datum'}." ".$1.":00");
-    }
-
-    if ($orga=~/Beginn:?\s+(\d+)[:\.](\d+)/) {
-        $event->{'beginn'}=$datumFormat->parse_datetime($event->{'datum'}." ".$1.":".$2);
-    }
-    elsif ($orga=~/(\d+)[:\.](\d+) (?:Uhr )?Beginn/) {
-        $event->{'beginn'}=$datumFormat->parse_datetime($event->{'datum'}." ".$1.":".$2);
-    }
-    elsif ($orga=~/Beginn:?\s+(\d+)[:\.]/) {	# Minuten "00" wird gelegenlich als "oo" geschrieben...
-        $event->{'beginn'}=$datumFormat->parse_datetime($event->{'datum'}." ".$1.":00");
-    }
-    elsif ($orga=~/(\d+)[:\.].{2} Beginn/) {	# Minuten "00" wird gelegenlich als "oo" geschrieben, und "Beginn" danach
-        $event->{'beginn'}=$datumFormat->parse_datetime($event->{'datum'}." ".$1.":00");
-    }
-    elsif ($orga=~/Beginn:? (\d+) Uhr/) {	# Minuten werden gelegentlich weggelassen
-        $event->{'beginn'}=$datumFormat->parse_datetime($event->{'datum'}." ".$1.":00");
-    }
-
-    if (!$event->{'einlass'} and !$event->{'beginn'}) {
-	# 15 – 22 Uhr, 15-22:14 Uhr, 14:15 bis 16 Uhr,... - aus HTML kratzen
-	if ($text_html=~/(\d+):?(\d{2})?\s*(?:bis|[\-–]|&ndash;|&dash;)\s*(\d+):?(\d{2})?\s+Uhr/i) {
-	    $event->{'beginn'}=$datumFormat->parse_datetime($event->{'datum'}." ".$1.":".($2//"00"));
-	    $event->{'ende'}=$datumFormat->parse_datetime($event->{'datum'}." ".$3.":".($4//"00"));
+	my $event;
+	# Unterseite aufrufen
+	try {
+	    $event->{'url'}=$eventItem->look_down('_tag'=>'a')->attr('href');
+	    $mech->get($event->{'url'});
 	}
-    }
+	catch {
+	    next;
+	};
 
-    # Wenn überhaupt keine Zeitangabe gefunden werden konnte: Als Ganztagesevent eintragen
-    if (!$event->{'einlass'} and !$event->{'beginn'}) {
-	#die($event->{'url'});
-	$event->{'beginn'}=$datumFormat->parse_datetime($event->{'datum'}." 00:00");
-	$event->{'ende'}=$datumFormat->parse_datetime($event->{'datum'}." 23:59");
-    }
 
-    # Fehlende Zeitangaben ergänzen
-    $event->{'einlass'}=$event->{'beginn'} unless ($event->{'einlass'});
-    $event->{'beginn'}=$event->{'einlass'} unless ($event->{'beginn'});
-    unless ($event->{'ende'}) {
-	$event->{'ende'}=$event->{'beginn'}->clone();
-	$event->{'ende'}->add(minutes=>$defaultDauer);
-    }
+	my $page=HTML::TreeBuilder->new();
+	$page->ignore_unknown(0);       # "article"-Tag wird sonst nicht erkannt
+	$page->parse_content($mech->content());
 
-    push (@eventList,$event);
+	### Kopfzeile: TITEL MONAT TAG WOCHENTAG HH:MM KATEGORIE
+	my $headline=$page->look_down('_tag'=>'div','class'=>"container is-fluid");
+	$headline=$headline->look_down('_tag'=>'div','class'=>qr/columns/);
+	# Titel
+	$event->{'titel'}=$headline->look_down('_tag'=>'h1')->as_trimmed_text();
+	# und der Rest
+	my ($monatsname,$tag,$wochentag,$stunde,$minute,$kategorien)=$headline->as_HTML()=~/<span class="is-date">\s*(\w\w\w)\s+(\d+)\s*<\/span>\s*<span>(\w+)\s+(\d+)[\.: ](\d+)<\/span>\s*<span>(.*)<\/span>/;
+	# Datum - leider fehlt das Jahr
+	my $now=DateTime->now();
+	my $jahr=$now->strftime("%Y");
+	my $monat=1+first_index { $_ eq lc($monatsname) } @monatsnamen;
+	$event->{'einlass'}=$datumFormat->parse_datetime($tag.".".$monat.".".$jahr." ".$stunde.":".$minute);
+	# wenn z.B. durch Jahreswechsel die so erstellte Einlasszeit zu weit in der Vergangenheit liegt: 1 Jahr dazuzuzählen
+	if ($event->{'einlass'}->add(months => -1) < $now) {
+	    $event->{'einlass'}=$event->{'einlass'}->add(years => 1);
+	}
+	$event->{'beginn'}=$event->{'einlass'}->clone();
+
+	@{$event->{'kategorien'}} = split /,\s*/, $kategorien;
+
+	### Fußzeile: Tickets
+	my $ticketlink=$page->look_down('_tag'=>'a','class'=>qr/event__tickets/);
+	if ($ticketlink) {
+	    $event->{'tickets'}=$ticketlink->attr('href');
+	}
+
+
+	## Beschreibungstext ("content")
+	my $content=$page->look_down('_tag'=>'div','class'=>'content');
+	my $text_html=$content->as_HTML();
+	my $text=$content->as_trimmed_text();
+
+	# Beschreibungstext hat normalerweise zwei Teile: Inhaltsbeschreibung und, nach ein paar Tildezeichen, Organisatorisches
+	my $orga;
+	if ($text=~/(.+?)\~\~\~\~\~\~+(.+)/) {
+	    $event->{'beschreibung'}=$1;
+	    $orga=$2;
+	}
+	else {
+	    # Ohne Trenner kompletten Text als Beschreibung verwenden..
+	    $event->{'beschreibung'}=$text;
+	    # ...und versuchen, daraus Einlass, Beginn und Preise rauszufinden
+	    $orga=$text;
+	}
+
+
+	# "Organisatorisches" enthält Datum, AK, VVK, Einlass, Beginn usw.
+	# Datum von oben wenn möglich überschreiben
+	#if ($orga=~/^\s*(\d{2}\.\d{2}\.\d{4})/) {
+	#    $event->{'datum'}=$1;
+	#}
+
+	if ($orga=~/(VVK:?\s+[\d,\.]+\s€ zzgl\. Geb\.)/) {
+	    $event->{'vvk'}=$1;
+	}
+	if ($orga=~/(AK:?\s+[\d,\.]+\s€)/) {
+	    $event->{'ak'}=$1;
+	}
+
+	if ($orga=~/Einlass:?\s+(\d+)[:\.](\d+)/) {
+	    $event->{'einlass'}->set(hour=>$1, minute=>$2);
+	}
+	elsif ($orga=~/(\d+)[:\.](\d+) (?:Uhr )?Einlass/) {
+	    $event->{'einlass'}->set(hour=>$1, minute=>$2);
+	}
+	elsif ($orga=~/Einlass:?\s+(\d+)[:\.]/) {	# Minuten "00" wird gelegenlich als "oo" geschrieben...
+	    $event->{'einlass'}->set(hour=>$1, minute=>0);
+	}
+	elsif ($orga=~/(\d+)[:\.].{2} Einlass/) {	# Minuten "00" wird gelegenlich als "oo" geschrieben, und "Einlass" danach
+	    $event->{'einlass'}->set(hour=>$1, minute=>0);
+	}
+
+	if ($orga=~/Beginn:?\s+(\d+)[:\.](\d+)/) {
+	    $event->{'beginn'}->set(hour=>$1, minute=>$2);
+	}
+	elsif ($orga=~/(\d+)[:\.](\d+) (?:Uhr )?Beginn/) {
+	    $event->{'beginn'}->set(hour=>$1, minute=>$2);
+	}
+	elsif ($orga=~/Beginn:?\s+(\d+)[:\.]/) {	# Minuten "00" wird gelegenlich als "oo" geschrieben...
+	    $event->{'beginn'}->set(hour=>$1, minute=>0);
+	}
+	elsif ($orga=~/(\d+)[:\.].{2} Beginn/) {	# Minuten "00" wird gelegenlich als "oo" geschrieben, und "Beginn" danach
+	    $event->{'beginn'}->set(hour=>$1, minute=>0);
+	}
+	elsif ($orga=~/Beginn:? (\d+) Uhr/) {	# Minuten werden gelegentlich weggelassen
+	    $event->{'beginn'}->set(hour=>$1, minute=>0);
+	}
+
+	if (!$event->{'einlass'} and !$event->{'beginn'}) {
+	    # 15 – 22 Uhr, 15-22:14 Uhr, 14:15 bis 16 Uhr,... - aus HTML kratzen
+	    if ($text_html=~/(\d+):?(\d{2})?\s*(?:bis|[\-–]|&ndash;|&dash;)\s*(\d+):?(\d{2})?\s+Uhr/i) {
+		$event->{'beginn'}->set(hour=>$1, minute=>($2//"00"));
+		$event->{'ende'}->set(hour=>$3, minute=>($4//"00"));
+	    }
+	}
+
+	# Wenn überhaupt keine Zeitangabe gefunden werden konnte: Als Ganztagesevent eintragen
+	if (!$event->{'einlass'} and !$event->{'beginn'}) {
+	    #die($event->{'url'});
+	    $event->{'beginn'}=$datumFormat->parse_datetime($event->{'datum'}." 00:00");
+	    $event->{'ende'}=$datumFormat->parse_datetime($event->{'datum'}." 23:59");
+	}
+
+	# Fehlende Zeitangaben ergänzen
+	$event->{'einlass'}=$event->{'beginn'} unless ($event->{'einlass'});
+	$event->{'beginn'}=$event->{'einlass'} unless ($event->{'beginn'});
+	unless ($event->{'ende'}) {
+	    $event->{'ende'}=$event->{'beginn'}->clone();
+	    $event->{'ende'}->add(minutes=>$defaultDauer);
+	}
+
+	push (@eventList,$event);
+    }
 }
 
 # Create Datestamp for dtstamp
@@ -189,7 +212,7 @@ foreach my $event (@eventList) {
 	uid=>$uid,
 	summary => $event->{'titel'},
 	description => $description,
-	categories => $event->{'kategorie'},
+	categories => join(", ",@{$event->{'kategorien'}}),
 	dtstart => DateTime::Format::ICal->format_datetime(
 	    DateTime->new(
 		year=>$event->{'beginn'}->year,
