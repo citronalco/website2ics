f9edfe537ece20a44a0aaf5c015d3d449252f6cc
Bernhard Geier <2651739+citronalco@users.noreply.github.com>
2023-11-19 21:37:01 +0100

    Neu: ingolstadt-kulturamt.pl

---
+++ ingolstadt-kulturamt.pl
@@ -0,0 +1,46 @@
+#!/usr/bin/perl
+# geierb@geierb.de
+# GPLv3
+
+use strict;
+use WWW::Mechanize;
+
+use DateTime;
+
+use Data::ICal;
+use Data::ICal::Entry::Event;
+
+use utf8;
+use warnings;
+
+# Kulturamt Ingolstadt already provides basic information in ics format, but only for single months
+
+my $calendar=Data::ICal->new();
+$calendar->add_properties(method=>"PUBLISH",
+    "X-PUBLISHED-TTL"=>"P1D",
+    "X-WR-CALNAME"=>"Kulturamt Ingolstadt",
+    "X-WR-CALDESC"=>"Kulturamt Ingolstadt Veranstaltungen");
+
+# avoid "wide character" warnings
+binmode STDOUT, ":utf8";
+my $mech=WWW::Mechanize->new();
+
+my $date=DateTime->now;
+my $MAXMONTHS=12;
+for (my $month=0;$month<$MAXMONTHS;$month++) {
+    my $url="https://www.kulturamt-ingolstadt.de/veranstaltungen/monat/".$date->strftime("%Y-%m")."/?ical=1";
+
+    $mech->get($url) or die($!);
+
+    my $kaCalendar=Data::ICal->new(data=>$mech->content());
+
+    # if there are no events this month, no ical without entries, but unfortunately an empty document is returned
+    next unless $kaCalendar;
+
+    foreach my $entry (@{$kaCalendar->entries}) {
+	$calendar->add_entry($entry);
+    }
+    $date->add(months=>1);
+}
+
+print $calendar->as_string;
